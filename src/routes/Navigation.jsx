a982313d39a926d3ce949a415992047d4fd9f983
Borkeyda <borisencinas22@gmail.com>
2022-06-17 10:50:12 -0400

    Dashboard

--- src/routes/Navigation.jsx
+++ src/routes/Navigation.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {BrowserRouter, Routes, Route, NavLink, Navigate} from 'react-router-dom'
 import {Home} from '../components/Home'
 import {About} from '../components/About'
-
+import { Dashboard } from '../components/Dashboard'
 import logo from '../emi.jpg';
 
 function Navigation() {
@@ -21,12 +21,18 @@ function Navigation() {
                     About
                   </NavLink>
                 </li>
+                <li>
+                  <NavLink to='/dashboard' className={({isActive})=>isActive ? 'nav-active' : ''}>
+                    Dashboard
+                  </NavLink>
+                </li>
           </ul>
       </nav>
         <Routes>
             <Route path='home' element={<Home/>} />
             <Route path='/*' element={<Navigate to='/home' replace/> } />
             <Route path='about' element={<About/>} />
+            <Route path='dashboard' element={<Dashboard/>} />
         </Routes>
       </BrowserRouter>
     
